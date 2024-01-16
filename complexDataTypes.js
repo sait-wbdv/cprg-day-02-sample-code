843fdeb327ad2cec077dd6002c8d440a54d18467
Ashlyn Knox <ashlyn@lilyx.io>
2024-01-16 10:46:56 -0700

    naming refactor and complex types example

---
+++ complexDataTypes.js
@@ -0,0 +1,22 @@
+const users = [
+  {
+    userId: 1234,
+    firstName: "Ashlyn",
+    lastName: "Knox",
+    isActive: true,
+  },
+  {
+    userId: 1235,
+    firstName: "Copernicus",
+    lastName: "Sees the stars",
+    isActive: false,
+  },
+  {
+    userId: 1236,
+    firstName: "Sally",
+    lastName: "Fields",
+    isActive: true,
+  },
+];
+
+console.log(users[1].firstName, users[2].lastName);
