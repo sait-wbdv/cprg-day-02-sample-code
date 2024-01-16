e80b81b9001469a87785008c7c94301e7a8c1b9c
Ashlyn Knox <ashlyn@lilyx.io>
2024-01-16 13:02:25 -0700

    function code examples

---
+++ functions.js
@@ -0,0 +1,49 @@
+const users = [
+  {
+    userId: 1234,
+    firstName: "Ashlyn",
+    lastName: "Knox",
+    status: true,
+  },
+  {
+    userId: 1235,
+    firstName: "Copernicus",
+    lastName: "Sees the stars",
+    status: false,
+  },
+  {
+    userId: 1236,
+    firstName: "Sally",
+    lastName: "Fields",
+    status: true,
+  },
+];
+
+// function declaration
+function printUser(user) {
+  console.log(user);
+}
+
+function getFullName(user) {
+  return user.firstName + " " + user.lastName;
+}
+
+function isUserStatusActive(user) {
+  if (user.status === true) {
+    return true;
+  } else {
+    return false;
+  }
+}
+let activeUser = isUserStatusActive(users[0]);
+
+console.log(activeUser);
+
+// function invocation
+
+isUserStatusActive(users[0]);
+
+// invoke and assign the result to a variable
+const userTwoName = getFullName(users[2]);
+// log the variable
+console.log(userTwoName);
